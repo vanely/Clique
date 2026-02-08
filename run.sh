ea939623bd712c9477cbb44554e97b1603c21467
vanely <vanelys94@gmail.com>
2026-02-08 15:40:12 -0500

    c-notes own run script

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Simple launcher - runs the same PowerShell command used to start the app
-
-powershell.exe -Command "Start-Process 'C:\Users\vanel\Projects\click-automation\ahk-clique\src\main.ahk'"
