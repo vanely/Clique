4a5e6e9b0d65853ca4def61c291a4069d67f5785
vanely <vanelys94@gmail.com>
2026-02-09 17:51:45 -0500

    phil path launch

--- c-note-run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Simple launcher - runs the same PowerShell command used to start the app
-
-powershell.exe -Command "Start-Process 'C:\Users\cedon\Projects\click-automation\ahk-clique\src\main.ahk'"
---
+++ phil.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Simple launcher - runs the same PowerShell command used to start the app
+
+powershell.exe -Command "Start-Process 'C:\Users\ilike\Projects\click-automation\ahk-clique\src\main.ahk'"
